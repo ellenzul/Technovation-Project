b83ee9dd6fa89ca1d791b129280511bdc5708e71
Mary Xu <xu.ruili99@gmail.com>
2021-02-28 12:12:05 -0800

    Fixed app setup

--- FoodShare/app/src/main/java/com/example/foodshare/MainActivity.java
+++ FoodShare/app/src/main/java/com/example/foodshare/MainActivity.java
@@ -15,22 +15,20 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView itemsRecyclerView;
     private ItemAdapter itemAdapter;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        itemAdapter = new ItemAdapter(foodList);
 
+        foodList = new ArrayList<>();
         TaskItem fakeItem = new TaskItem("Food1", "Description of food", "Peanuts", "123 Mohave Terrace");
         foodList.add(fakeItem);
         TaskItem fakeItem2 = new TaskItem("Food2", "Description of food2!", "None", "Local store");
         foodList.add(fakeItem2);
 
-
         itemsRecyclerView = findViewById(R.id.itemsRecyclerView);
         itemsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        itemAdapter = new ItemAdapter(foodList);
         itemsRecyclerView.setAdapter(itemAdapter);
     }
 }
