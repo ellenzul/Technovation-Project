f51421c818ebc43b00c3dc85792a6f0eb1f547fc
Ruby <rxu456@gmail.com>
2021-03-02 22:31:34 -0800

    Commit redo oop

---
+++ app/src/main/java/com/example/foodshare/ItemAdapter.java
@@ -0,0 +1,69 @@
+package com.example.foodshare;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import com.example.foodshare.models.TaskItem;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+    List<TaskItem> mItems;
+    Context context;
+
+    public ItemAdapter(List<TaskItem> items) {
+        mItems = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View itemView = inflater.inflate(R.layout.listing, parent, false);
+        ViewHolder viewHolder = new ViewHolder(itemView);
+        return viewHolder;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        // get data according to position
+        final TaskItem item = mItems.get(position);
+        // populate the views according to this data
+        holder.titleTextview.setText(item.title);
+        holder.descriptionTextview.setText(item.description);
+        holder.tagsTextview.setText(item.tags);
+        holder.locationTextview.setText(item.location);
+        holder.foodImage.setImageBitmap(item.image);
+    }
+
+    @Override
+    public int getItemCount() { return mItems.size(); }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView titleTextview;
+        public TextView descriptionTextview;
+        public TextView tagsTextview;
+        public TextView locationTextview;
+        public ImageView foodImage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            titleTextview = itemView.findViewById(R.id.foodName);
+            descriptionTextview = itemView.findViewById(R.id.foodDescription);
+            tagsTextview = itemView.findViewById(R.id.foodTags);
+            locationTextview = itemView.findViewById(R.id.foodLocation);
+            foodImage = itemView.findViewById(R.id.foodImage);
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/foodshare/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.foodshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import com.example.foodshare.models.TaskItem;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<TaskItem> foodList;
+    private RecyclerView itemsRecyclerView;
+    private ItemAdapter itemAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        foodList = new ArrayList<>();
+        TaskItem fakeItem = new TaskItem("Food1", "Description of food", "Peanuts", "123 Mohave Terrace");
+        fakeItem.image = BitmapFactory.decodeResource(this.getResources(),R.drawable.foodimage);
+        foodList.add(fakeItem);
+        TaskItem fakeItem2 = new TaskItem("Food2", "Description of food2!", "None", "Local store");
+        fakeItem2.image = BitmapFactory.decodeResource(this.getResources(),R.drawable.foodimage);
+        foodList.add(fakeItem2);
+
+
+        itemsRecyclerView = findViewById(R.id.itemsRecyclerView);
+        itemsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        itemAdapter = new ItemAdapter(foodList);
+        itemsRecyclerView.setAdapter(itemAdapter);
+    }
+}
