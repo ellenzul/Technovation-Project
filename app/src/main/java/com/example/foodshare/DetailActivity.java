90fe9037db477d8ef70244383064113746070d3f
Ellen Zulkarnain <ellenzul18@gmail.com>
2021-03-23 22:07:38 -0700

    detail activty and confirmation btn

--- app/src/main/java/com/example/foodshare/DetailActivity.java
+++ app/src/main/java/com/example/foodshare/DetailActivity.java
@@ -2,7 +2,9 @@ package com.example.foodshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,23 +18,38 @@ public class DetailActivity extends AppCompatActivity {
     private Button btnComplete;
     private TextView tvTitle;
     private TextView tvDescription;
-    private ImageView imageView;
+    private ImageView itemimageView;
     private TaskItem item;
+    private Button btnConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        tvTitle.setText(item.title);
-        tvDescription.setText(item.description);
-        imageView.setImageBitmap(item.image);
-
         btnComplete = findViewById(R.id.btnComplete);
         tvTitle = findViewById(R.id.tvTitle);
         tvDescription = findViewById(R.id.tvDescription);
-        imageView = findViewById(R.id.itemImageView);
+        itemimageView = findViewById(R.id.itemImageView);
+        btnConfirm = findViewById(R.id.btnConfirm);
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, ConfirmationActivity.class);
+        startActivity(intent);
+
         item = Parcels.unwrap(getIntent().getParcelableExtra(TaskItem.class.getSimpleName()));
+
+        tvTitle.setText(item.title);
+        tvDescription.setText(item.description);
+        itemimageView.setImageBitmap(item.image);
 
     }
 }
